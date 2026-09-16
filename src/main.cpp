d9eb97c6640e3a444fdca90be74c6274824c79a8
filipeparedes <filipeparedes3@gmail.com>
2026-09-16 02:25:30 +0100

    feature: add main entry point wiring HttpServer

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,26 @@
+/**
+ * @file main.cpp
+ * @brief Entry point for the Nubilo backend.
+ *
+ * @author Filipe Paredes (filipeparedes3@gmail.com)
+ *
+ * @copyright Copyright (c) 2026
+ */
+
 import version;
+import HttpServer;
 
-#include <iostream>
+#include <print>
 
+/**
+ * @brief Starts the Nubilo backend server.
+ * @return 0 on normal exit.
+ */
 int main() {
-    std::cout << "Nubilo backend " <<nubilo::getVersion() << " starting...\n";
+    std::println("Nubilo backend {} starting...", nubilo::getVersion());
+
+    nubilo::HttpServer server(8080);
+    server.run();
+
     return 0;
 }
