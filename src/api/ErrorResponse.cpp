a6ce89e44a3a5d1a51125af018978981780472e8
filipeparedes <filipeparedes3@gmail.com>
2026-09-20 22:00:42 +0100

    feature: add writeErrorResponse helper for consistent API error format

---
+++ src/api/ErrorResponse.cpp
@@ -0,0 +1,17 @@
+#include "api/ErrorResponse.h"
+
+#include <nlohmann/json.hpp>
+
+namespace nubilo {
+
+void writeErrorResponse(httplib::Response& res, int httpStatus, const std::string& code, const std::string& message) {
+    nlohmann::json body = {
+        {"error", {
+                {"code", code},
+                {"message", message}
+        }}
+    };
+
+    res.status = httpStatus;
+    res.set_content(body.dump(), "application/json");
+}
